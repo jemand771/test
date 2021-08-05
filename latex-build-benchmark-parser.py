0a7004a412ba7c672ebf874e2ca3a93095d0f111
Willy <jemand771@gmx.net>
2021-08-05 07:40:59 +0000

    column headers that actually make sense

--- latex-build-benchmark-parser.py
+++ latex-build-benchmark-parser.py
@@ -9,13 +9,18 @@ def main():
     lines = {}
     for r in results:
         tag, *args, elapsed = r.split(",")
-        args = "-".join(args)
+        args = text_header(args)
         lines.setdefault(tag, [])
         lines[tag].append(elapsed)
         if args not in header:
             header.append(args)
     lines = [[k, *v] for k, v in lines.items()]
     print(tabulate(lines, headers=header, tablefmt="pretty"))
+
+
+def text_header(args):
+    
+    return ",".join(x for x, control in zip(["diff-pdf", "synctex", "pythontex"], args) if control == "true")
 
 
 if __name__ == "__main__":
